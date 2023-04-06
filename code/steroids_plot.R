b1ca1d8a152fcbacbae296025b2a61702c1aca93
Tianyi Li <tianyi_li1@sina.com>
2023-04-06 17:20:53 +0200

    Update code for new analysis

--- code/steroids_plot.R
+++ code/steroids_plot.R
@@ -1,5 +1,9 @@
-setwd("/Users/tili/Desktop/STAT_ovary/data/")
+
+setwd("/Users/tianyi.li.2/Desktop/STAT_ovary/data/")
 df <- read.csv("Steroids_CE groups.csv", header = TRUE, sep = ";", dec = ",")
+df <- read.csv("/Users/tianyi.li.2/Desktop/STAT_ovary/data/E18_steroid_results.csv", 
+               stringsAsFactors = F,
+               header = TRUE, sep = ";", dec = ",")
 
 library(ggplot2)
 library(stringr)
@@ -37,6 +41,7 @@ for (i in 6:15) {
 colnames(normalized) <- colnames(df)[c(2, 6:15)]
 normalized$IVA <- df$IVA 
 normalized$IVAs <- ifelse(str_detect(normalized$IVA, "0"), "No", "Yes")
+df$IVA <- ifelse(str_detect(df$IVA, "0"), "Fragmentation", "IVA")
 
 plot_list <- list()
 for (i in 2:11) {
@@ -58,3 +63,60 @@ for (i in 2:11) {
   print(t.test(log10(temp[,2] + 1) ~ temp$IVAs))
 }
 
+# not normalized to follicle number
+plot_list <- list()
+for (i in 6:15) {
+  temp <- df[,c(5,i)]
+  temp[is.infinite(temp[,2]),2] <- 0
+  temp$IVA <- factor(temp$IVA, levels = c("Fragmentation", "IVA"))
+  plot_list[[colnames(temp)[2]]] <- temp %>% ggplot(aes(x = IVA, y = log10(.[,2]+1), fill=IVA)) + geom_boxplot() + theme + 
+    ggtitle(colnames(temp)[2]) + ylab("Log10(Concentration+1)") + xlab("") + 
+    guides(fill = guide_legend(title = "Groups"))  + ylim(c(0, 2.5)) +
+    scale_fill_manual(values = c("#8dd3c7", "#fb8072"))
+}
+wrap_plots(plot_list[c(10,7,8,5,6,4)], nrow = 2, ncol = 3) + plot_layout(guides = "collect") + plot_annotation(tag_levels = "A")
+
+df <- df %>% mutate(gr = paste0(group, "_", Time))
+df$gr <- factor(df$gr, levels = c("Fragmentation_Day_1", "IVA_Day_1",
+                                  "Fragmentation_Day_3", "IVA_Day_3",
+                                  "Fragmentation_Day_5", "IVA_Day_5",
+                                  "Fragmentation_Day_7", "IVA_Day_7"))
+p1 <- ggplot(df, aes(x = gr, y = log10(as.numeric(pregnenolone)+1), fill=group)) + geom_boxplot() + theme  + 
+  ggtitle("Pregnenolone") + ylab("Log10(Concentration+1)") + xlab("") + 
+  guides(fill = guide_legend(title = "Groups")) + 
+  scale_fill_manual(values = c("#8dd3c7", "#fb8072"))
+
+p2 <- ggplot(df, aes(x = gr, y = log10(as.numeric(progesterone)+1), fill=group)) + geom_boxplot() + theme  + 
+  ggtitle("Progesterone") + ylab("Log10(Concentration+1)") + xlab("") + 
+  guides(fill = guide_legend(title = "Groups")) + 
+  scale_fill_manual(values = c("#8dd3c7", "#fb8072"))
+
+p3 <- ggplot(df, aes(x = gr, y = log10(as.numeric(DHEA_normalized)+1), fill=group)) + geom_boxplot() + theme  + 
+  ggtitle("DHEA") + ylab("Log10(Concentration+1)") + xlab("") + 
+  guides(fill = guide_legend(title = "Groups")) + 
+  scale_fill_manual(values = c("#8dd3c7", "#fb8072"))
+
+p4 <- ggplot(df, aes(x = gr, y = log10(as.numeric(androstenedione)+1), fill=group)) + geom_boxplot() + theme  + 
+  ggtitle("Androstenedione") + ylab("Log10(Concentration+1)") + xlab("") + 
+  guides(fill = guide_legend(title = "Groups")) + 
+  scale_fill_manual(values = c("#8dd3c7", "#fb8072"))
+
+p5 <- ggplot(df, aes(x = gr, y = log10(as.numeric(T_normalized)+1), fill=group)) + geom_boxplot() + theme  + 
+  ggtitle("Testosterone") + ylab("Log10(Concentration+1)") + xlab("") + 
+  guides(fill = guide_legend(title = "Groups")) + 
+  scale_fill_manual(values = c("#8dd3c7", "#fb8072"))
+
+p6 <- ggplot(df, aes(x = gr, y = log10(as.numeric(E2_normalized)+1), fill=group)) + geom_boxplot() + theme  + 
+  ggtitle("Estradiol") + ylab("Log10(Concentration+1)") + xlab("") + 
+  guides(fill = guide_legend(title = "Groups")) + 
+  scale_fill_manual(values = c("#8dd3c7", "#fb8072"))
+
+p1 + p2 + p3 + p4 + p5 + p6 + plot_annotation(tag_levels = "A") + plot_layout(ncol = 3, guides = "collect")
+
+
+################ Statistics ###############
+
+d5 <- df %>% select(gr, pregnenolone)
+d5[,2] <- as.numeric(d5[, 2])
+an <- aov(log10(d5$pregnenolone + 1) ~ d5$gr)  
+TukeyHSD(an)  
